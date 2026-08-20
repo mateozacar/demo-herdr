4d9f5941dd3e64516f075c750651e70acde635bc
mateozapata <mateozacar@gmail.com>
2026-08-20 13:09:43 -0500

    feat(20260820_130718): implement validateCreditCard

--- utils.js
+++ utils.js
@@ -49,7 +49,48 @@ function _result(valid, fields) {
 // Luhn algorithm + card type detection (Visa/Mastercard/Amex)
 // Returns: { valid: boolean, type: string|null, masked: string|null }
 function validateCreditCard(number) {
-  throw new Error("Not implemented");
+  if (!_checkInput(number, "string")) {
+    return _result(false, { type: null, masked: null });
+  }
+
+  const cleaned = number.replace(/[\s-]/g, "");
+  if (!/^\d+$/.test(cleaned) || cleaned.length < 13 || cleaned.length > 19) {
+    return _result(false, { type: null, masked: null });
+  }
+
+  let type = null;
+  if (/^4/.test(cleaned)) {
+    type = "Visa";
+  } else if (/^5[1-5]/.test(cleaned)) {
+    type = "Mastercard";
+  } else if (/^3[47]/.test(cleaned)) {
+    type = "Amex";
+  }
+
+  if (!type) {
+    return _result(false, { type: null, masked: null });
+  }
+
+  let sum = 0;
+  let shouldDouble = false;
+  for (let i = cleaned.length - 1; i >= 0; i--) {
+    let digit = parseInt(cleaned.charAt(i), 10);
+    if (shouldDouble) {
+      digit *= 2;
+      if (digit > 9) {
+        digit -= 9;
+      }
+    }
+    sum += digit;
+    shouldDouble = !shouldDouble;
+  }
+
+  if (sum % 10 !== 0) {
+    return _result(false, { type: null, masked: null });
+  }
+
+  const masked = `****-****-****-${cleaned.slice(-4)}`;
+  return _result(true, { type, masked });
 }
 
 // TODO: implement validatePasswordStrength(password)
