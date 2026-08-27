f27132e4c0c50c8c10dd33af9154f9c5e888490d
mateozapata <mateozacar@gmail.com>
2026-08-27 12:34:58 -0500

    feat(creditcard): implement validateCreditCard

--- utils.js
+++ utils.js
@@ -49,7 +49,42 @@ function _result(valid, fields) {
 // Luhn algorithm + card type detection (Visa/Mastercard/Amex)
 // Returns: { valid: boolean, type: string|null, masked: string|null }
 function validateCreditCard(number) {
-  throw new Error("Not implemented");
+  if (!_checkInput(number, "string")) {
+    return _result(false, { type: null, masked: null });
+  }
+
+  const cleaned = number.replace(/[\s-]/g, "");
+  if (!/^\d+$/.test(cleaned)) {
+    return _result(false, { type: null, masked: null });
+  }
+
+  let type = null;
+  if (/^4/.test(cleaned)) {
+    type = "Visa";
+  } else if (/^5[1-5]/.test(cleaned)) {
+    type = "Mastercard";
+  } else if (/^3[47]/.test(cleaned)) {
+    type = "Amex";
+  }
+
+  let sum = 0;
+  let shouldDouble = false;
+  for (let i = cleaned.length - 1; i >= 0; i--) {
+    let digit = parseInt(cleaned[i], 10);
+    if (shouldDouble) {
+      digit *= 2;
+      if (digit > 9) digit -= 9;
+    }
+    sum += digit;
+    shouldDouble = !shouldDouble;
+  }
+
+  const isValid = type !== null && sum % 10 === 0;
+
+  return _result(isValid, {
+    type: isValid ? type : null,
+    masked: isValid ? `****-****-****-${cleaned.slice(-4)}` : null,
+  });
 }
 
 // TODO: implement validatePasswordStrength(password)
